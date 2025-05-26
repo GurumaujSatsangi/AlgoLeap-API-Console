8efa41da7e55b092109ae27d173f65f7aefbbf56
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2025-05-26 15:55:37 +0530

    issues fixed

--- index.js
+++ index.js
@@ -178,14 +178,11 @@ const {prompt,apiKey} = req.query;
     },
   });
   for (const part of response.candidates[0].content.parts) {
-    // Based on the part type, either show the text or save the image
     if (part.text) {
       console.log(part.text);
     } else if (part.inlineData) {
       const imageData = part.inlineData.data;
-      const buffer = Buffer.from(imageData, "base64");
-      fs.writeFileSync("image.png", buffer);
-      console.log("Image saved as image.png");
+      res.send(imageData);
     }
   }
 
